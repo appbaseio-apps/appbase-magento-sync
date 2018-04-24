5ade60100c376551b612d050e21d348b9551a86b
Rishabh Jain <rishabh@jnexsoft.com>
2018-04-25 03:30:44 +0530

    Fixed update products and added resync date

--- update.php
+++ update.php
@@ -4,15 +4,22 @@ try {
 
   $date = $appbase->getLastSyncTimestamp();
 
-  $products = ($megento->getProductsAfter($date)->items);
+  $products = ($magento->getProductsAfter($date)->items);
 
   if (count($products) === 0) {
     throw new Exception("No products found.");
   }
 
+  $timestamp = (productsMaxUpdatedAt($products));
+  $bulk = [];
   foreach ($products as $product) {
-    $appbase->addProduct($bulk);
+    $id = $product->sku;
+    array_push($bulk, ["index" => [ "_type" => "products", "_id" => $id ]]);
+    array_push($bulk, $product);
   }
+  $appbase->addProducts($bulk);
+
+  $appbase->setSyncTimestamp($timestamp);
 
 } catch (Exception $e) {
   echo $e->getMessage();
